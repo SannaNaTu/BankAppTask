001732aea3f2a1899374736eb8ffb72c11669dc9
SannaNaTu <korhonsm@gmail.com>
2018-04-26 00:25:45 +0300

    update done

--- BankAppTask/BankAppTask/Repositories/BankRepository.cs
+++ BankAppTask/BankAppTask/Repositories/BankRepository.cs
@@ -11,22 +11,22 @@ namespace BankAppTask.Repositories
     {
         private static BankdbContext _context = new BankdbContext();
 
-        //Tulosta pankit
-        public List<Bank> GetTransactionsFromBankCustomersAccounts()
-        {
-            List<Bank> banks = _context.Bank
-                .Include(b => b.Customer)
-                .Include(b => b.Account)
-                .Include(b => b.Account).ThenInclude(a => a.Transaction)
-                .ToListAsync().Result;
-            return banks;
-        }
+        ////Tulosta pankit
+        //public List<Bank> GetTransactionsFromBankCustomersAccounts()
+        //{
+        //    List<Bank> banks = _context.Bank
+        //        .Include(b => b.Customer)
+        //        .Include(b => b.Account)
+        //        .Include(b => b.Account).ThenInclude(a => a.Transaction)
+        //        .ToListAsync().Result;
+        //    return banks;
+        //}
   
-        public Bank GetBankById(long id) //Eti pankki
-        {
-            var bank = _context.Bank.FirstOrDefault(b => b.Id == id);
-            return bank;
-        }
+        //public Bank GetBankById(long id) //Eti pankki
+        //{
+        //    var bank = _context.Bank.FirstOrDefault(b => b.Id == id);
+        //    return bank;
+        //}
 
         public  void Create(Bank bank) //Lisää
         {
